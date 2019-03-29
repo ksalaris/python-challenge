adb3d1b2a6b5a6f61f203c4a802385c5864b9a5e
Katie Salaris <ksalaris@gmail.com>
2019-03-29 00:14:51 -0400

    Updated pypoll method

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,64 +19,37 @@ with open(csvpath, newline='') as csvfile:
         if row[2] not in candidates:
             candidates.append(row[2])
     
-    khan = 0
-    correy = 0
-    li = 0
-    otooley = 0
-    
-    for row in data:
-        if row[2] == candidates[0]:
-            khan += 1
-        if row[2] == candidates[1]:
-            correy += 1
-        if row[2] == candidates[2]:
-            li += 1
-        if row[2] == candidates[3]:
-            otooley += 1
-    
-                
-    #The percentage of votes each candidate won
-    pk = 0
-    pc = 0
-    pl = 0
-    po = 0
-    
-    pk = round(((khan/total_votes)*100),2)
-    pc = round(((correy/total_votes)*100),2)
-    pl = round(((li/total_votes)*100),2)
-    po = round(((otooley/total_votes)*100),2)
-    
-
-    #The winner of the election based on popular vote.
-    votes = [khan, correy, li, otooley]
-    candidates_votes = dict(zip(candidates,votes))
-    
+    percentages = []
+    vote_counts=[]
+    all_votes=[]
+    for row in candidates:
+        for i in range(0,total_votes,1):
+            if row == data[i][2]:
+                vote_counts.append(data[0])
+        all_votes.append(len(vote_counts))
+        percentages.append(round(((len(vote_counts))/total_votes)*100,3))
+        vote_counts.clear()
+        
+    candidates_votes = dict(zip(candidates,all_votes))
     winner = max(candidates_votes.keys(),key=lambda v: candidates_votes[v]) 
-    print(winner)
-
+    
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
-print(f"Khan: {pk}% ({khan})")
-print(f"Correy: {pc}% ({correy})")
-print(f"Li: {pl}% ({li})")
-print(f"O'Tooley: {po}% ({otooley})")
+for i in range(0,len(candidates),1):
+    print(f"{candidates[i]}: {percentages[i]}% ({all_votes[i]}) ")
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
-
-
 
 file = open("output.txt","w")
 file.write("Election Results"+ '\n')
 file.write("-------------------------"+ '\n')
 file.write(f"Total Votes: {total_votes}"+ '\n')
 file.write("-------------------------"+ '\n')
-file.write(f"Khan: {pk}% ({khan})"+ '\n')
-file.write(f"Correy: {pc}% ({correy})"+ '\n')
-file.write(f"Li: {pl}% ({li})"+ '\n')
-file.write(f"O'Tooley: {po}% ({otooley})"+ '\n')
+for i in range(0,len(candidates),1):
+    file.write(f"{candidates[i]}: {percentages[i]}% ({all_votes[i]}) "+ '\n')
 file.write("-------------------------"+ '\n')
 file.write(f"Winner: {winner}"+ '\n')
 file.write("-------------------------"+ '\n')
